ef0ef5b64cee23e4ae6e96766600d41cdf219c59
Ayumu Oshiro <95387137+RealCntAyumu@users.noreply.github.com>
2023-02-28 21:21:32 -0800

    Update OverviewPage.java

--- OverviewPage.java
+++ OverviewPage.java
@@ -1,37 +1,123 @@
-package teamViolet;
-
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-/***
+/**
  * 
- * @author Ayumu Oshiro(Class Template)
+ * @author Ayumu Oshiro 
  *
  */
 public class OverviewPage extends JPanel {
+
     public OverviewPage() {
+    	
+    	
+    	// Create a header panel
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+        JLabel headerLabel = new JLabel("Overview", SwingConstants.CENTER);
+        headerLabel.setFont(new Font("Helvetica", Font.BOLD, 32));
+        headerPanel.add(headerLabel, BorderLayout.CENTER);
+
+        JPanel backButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JButton backButton = new JButton("Back to Projects");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Go back to the subproject Page screen
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(OverviewPage.this);
+                CardLayout cardLayout = (CardLayout) frame.getContentPane().getLayout();
+                cardLayout.show(frame.getContentPane(), "SubprojectPage");
+
+                // Remove the SubprojectPage from the content pane
+                Container contentPane = frame.getContentPane();
+                contentPane.remove(OverviewPage.this);
+                contentPane.revalidate();
+                contentPane.repaint();
+            }
+        });
+        backButtonPanel.add(backButton);
+        headerPanel.add(backButtonPanel, BorderLayout.PAGE_END);
         
-        // add tabs on panel
+        add(headerPanel, BorderLayout.NORTH);
+        // Create a panel with tabs
         JTabbedPane tabbedPane = new JTabbedPane();
         add(tabbedPane);
 
-        JPanel overviewPanel = new JPanel();
-        JPanel budgetPanel = new JPanel();
-        JPanel itemPanel = new JPanel();
-
+        // Create the Overview tab
+        JPanel overviewPanel = new JPanel(new BorderLayout());
         tabbedPane.addTab("Overview", overviewPanel);
-        tabbedPane.addTab("Budget", budgetPanel);
-        tabbedPane.addTab("Item", itemPanel);
-        
-        // Overview screen component: 1: Budget Bar Graph 2: top 3 expenses box 3: item list box.
 
-        //Budget Screen components: 1: Budget Bar Graph, 2: cost input box and 3: cost estimation box
-       
-        // Item Screen components: file management system to add and delete documents
+        // Create the Budget tab
+        JPanel budgetPanel = new JPanel(new BorderLayout());
+        tabbedPane.addTab("Budget", budgetPanel);
+
+        // Create the Item tab
+        JPanel itemPanel = new JPanel(new BorderLayout());
+        tabbedPane.addTab("Item", itemPanel);
+
+        // Add components to the Overview tab
+        JPanel overviewTopPanel = new JPanel(new GridLayout(1, 2, 10, 10));
+        overviewTopPanel.setBorder(new EmptyBorder(20, 20, 0, 20));
+        overviewPanel.add(overviewTopPanel, BorderLayout.NORTH);
+
+        // Add the budget bar graph to the top panel
+        JPanel budgetGraphPanel = new JPanel();
+        budgetGraphPanel.setBackground(Color.WHITE);
+        budgetGraphPanel.setPreferredSize(new Dimension(300, 200));
+        budgetGraphPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        overviewTopPanel.add(budgetGraphPanel);
+
+        // Add the top 3 expenses panel to the top panel
+        JPanel expensesPanel = new JPanel(new BorderLayout());
+        expensesPanel.setBackground(Color.WHITE);
+        expensesPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        overviewTopPanel.add(expensesPanel);
+
+        JLabel expensesLabel = new JLabel("Top 3 Expenses", SwingConstants.CENTER);
+        expensesLabel.setFont(new Font("Helvetica", Font.BOLD, 16));
+        expensesPanel.add(expensesLabel, BorderLayout.NORTH);
+
+        // Add the expense items to the expenses panel
+        JPanel expenseItemsPanel = new JPanel(new GridLayout(3, 1, 0, 10));
+        expenseItemsPanel.setBackground(Color.WHITE);
+        expensesPanel.add(expenseItemsPanel, BorderLayout.CENTER);
+
+        for (int i = 0; i < 3; i++) {
+            JLabel expenseItemLabel = new JLabel("Expense Item " + (i + 1), SwingConstants.CENTER);
+            expenseItemLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            expenseItemsPanel.add(expenseItemLabel);
+        }
+
+        // Add the item list panel to the overview panel
+        JPanel itemListPanel = new JPanel(new BorderLayout());
+        itemListPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+        itemListPanel.setBackground(Color.WHITE);
+        itemListPanel.setPreferredSize(new Dimension(300, 400));
+        itemListPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        overviewPanel.add(itemListPanel, BorderLayout.CENTER);
+
+        JLabel itemListLabel = new JLabel("Item List", SwingConstants.CENTER);
+        itemListLabel.setFont(new Font("Helvetica", Font.BOLD, 16));
+        itemListPanel.add(itemListLabel, BorderLayout.NORTH);
+
+        // Add the item list to the item list panel
+        JPanel itemsPanel = new JPanel(new GridLayout(0, 1, 0, 10));
+        itemsPanel.setBackground(Color.WHITE);
+        JScrollPane itemsScrollPane = new JScrollPane(itemsPanel);
+        itemsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        itemsScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        itemListPanel.add(itemsScrollPane, BorderLayout.CENTER);
+
+        for (int i = 0; i < 10; i++) {
+            JLabel itemLabel = new JLabel("Item " + (i + 1));
+            itemLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            itemsPanel.add(itemLabel);
+        }
         
+
     }
 }
