a025430c93750c9c951aec61ef28bcecc760d19b
Abdirizak Ali <59887662+Abdirizak-byte@users.noreply.github.com>
2023-03-01 19:52:06 -0800

    Update Setting.java

--- Setting.java
+++ Setting.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Settings {
   private String firstName;
   private String email;
- 
+  private String information;
 
   public void setFirstName(String firstName) {
     this.firstName = firstName;
@@ -21,6 +21,38 @@ public class Settings {
     return email;
   }
 
- 
-}
+  public void setInformation(String information) {
+    this.information = information;
+  }
 
+  public String getInformation() {
+    return information;
+  }
+
+  public void exportSettings() {
+    System.out.println("Exporting Settings...");
+    System.out.println("First Name: " + firstName);
+    System.out.println("Email: " + email);
+  }
+
+  public void importSettings() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Importing Settings...");
+    System.out.print("Enter First Name: ");
+    firstName = scanner.nextLine();
+    System.out.print("Enter Email: ");
+    email = scanner.nextLine();
+  }
+
+  public void exportInformation() {
+    System.out.println("Exporting Information...");
+    System.out.println(information);
+  }
+
+  public void importInformation() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Importing Information...");
+    System.out.print("Enter Information: ");
+    information = scanner.nextLine();
+  }
+}
