3557f6ceb9dbd0b2e354bfbe01485b0fac9e117f
Abdirizak Ali <59887662+Abdirizak-byte@users.noreply.github.com>
2023-03-02 17:58:02 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  */
 public class Main {
 	  public static void main(String[] args) {
-	    Settings settings = new Settings();
+	    Setting settings = new Setting();
 	    Scanner scanner = new Scanner(System.in);
 
 	    System.out.print("Enter First Name: ");
