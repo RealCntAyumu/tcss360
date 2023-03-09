6bdd2f96e683ffb6a9ffc906f41912499d306a8c
ijneb <benji.i.lee@gmail.com>
2023-03-08 17:10:25 -0800

    made changes to ItemScreen.java, OverviewPage.java, and Subproject.java

--- Subproject.java
+++ Subproject.java
@@ -1,27 +1,46 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * 
+ * @author Benji Lee
+ *
+ */
 public class Subproject {
     private String name;
+    // list to save documents saved.
+    private ArrayList<File> documents;
+    
+    public void setDocuments(ArrayList<File> documents) {
+		this.documents = documents;
+	}
+
+	// list to save items. 
+    private List<Item> items;
+    
+    
     public List<Item> getItems() {
 		return items;
 	}
-
-	public void setItems(List<Item> items) {
-		this.items = items;
+    
+	public ArrayList<File> getDocuments() {
+		return documents;
 	}
+    
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	private List<Item> items;
+	
 
    // private List<String> ;
 
     public Subproject(String name) {
         this.name = name;
         items = new ArrayList<>();
+        documents = new ArrayList<>();
        // this.tasks = new ArrayList<>();
     }
 
@@ -29,9 +48,12 @@ public class Subproject {
         return name;
     }
 
-    public void addItem(Item item) {
-    	items.add(item);
+    public void addDoc(File item) {
+    	documents.add(item);
     }
+
+
+    
 
    // public List<Task> getTasks() {
        // return tasks;
