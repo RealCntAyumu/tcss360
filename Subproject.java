9d366075cd155e770ca54c6815b68ee0a6484387
Abdirizak Ali <59887662+Abdirizak-byte@users.noreply.github.com>
2023-03-09 10:09:45 -0800

    Update Subproject.java

--- Subproject.java
+++ Subproject.java
@@ -8,70 +8,78 @@ import java.util.List;
  * @author Abdirizak Ali
  *
  */
- 
 public class Subproject {
     private String name;
-    // list to save documents saved.
     private ArrayList<File> documents;
-    // list to save items. 
     private List<Item> items;
-
-    public List<Item> getItems() {
-        return items;
-    }
     
-	public ArrayList<File> getDocuments() {
-		return documents;
-	}
-    
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-// private List<String> ;
     public Subproject(String name) {
         this.name = name;
         items = new ArrayList<>();
         documents = new ArrayList<>();
-        // this.tasks = new ArrayList<>();
     }
-
+    
     public String getName() {
         return name;
     }
-    
-    public void addDocument(File document) {
-        documents.add(document);
+
+    public void setName(String name) {
+        this.name = name;
     }
     
     public ArrayList<File> getDocuments() {
         return documents;
     }
     
+    public void setDocuments(ArrayList<File> documents) {
+        this.documents = documents;
+    }
+
     public List<Item> getItems() {
         return items;
     }
-    
+
     public void addItem(Item item) {
         items.add(item);
     }
-    }
 
-public class OverviewPage {
-public void displaySubprojectDetails(Subproject subproject) {
-String subprojectName = subproject.getName();
-List<Item> subprojectItems = subproject.getItems();
-ArrayList<File> subprojectDocuments = subproject.getDocuments();
-    System.out.println("Subproject Name: " + subprojectName);
-    System.out.println("Subproject Items: ");
-    for (Item item : subprojectItems) {
-        System.out.println("- " + item.getName());
-    }
-
-    System.out.println("Subproject Documents: ");
-    for (File document : subprojectDocuments) {
-        System.out.println("- " + document.getName());
+    public void removeItem(Item item) {
+        items.remove(item);
     }
 }
+
+class Item {
+    private String name;
+    private String description;
+    private boolean completed;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+        completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
