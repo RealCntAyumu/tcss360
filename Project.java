243b98dff3f639cec032315a8e1dfdffd29c3947
Abdirizak Ali <59887662+Abdirizak-byte@users.noreply.github.com>
2023-03-16 12:35:34 -0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -3,32 +3,35 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Project {
-    private String projectName;
+    private String projectName; // Declaring a private field for storing project name
+    
+    // Setter method for setting project name
     public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
+        this.projectName = projectName;
+    }
 
-	private  HashMap<String, Subproject> subprojects;
-	
+    private  HashMap<String, Subproject> subprojects; // Declaring a private field for storing subprojects
 
+    // Constructor for creating a new Project instance with a given project name
     public Project(String projectName) {
         this.projectName = projectName;
         subprojects = new  HashMap<String, Subproject>();
     }
     
+    // Getter method for retrieving the project name
     public String getProjectName() {
-    	return projectName;
+        return projectName;
     }
 
+    // Method for adding a new subproject to the project
     public void addSubproject(Subproject subproject) {
         subprojects.put(subproject.getName(), subproject);
     }
 
+    // Getter method for retrieving all subprojects of the project
     public  HashMap<String, Subproject> getSubprojects() {
         return subprojects;
     }
     
     
-
-    // Other getters and setters
 }
